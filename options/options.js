fbf62253a1ce2f60bede226b24d0bc2e16ccbc2c
Crowdbotics <team@crowdbotics.com>
2024-06-10 09:02:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-voice-48331",
+  url: "https://flat-voice-48331.botics.co",
+  api: "https://flat-voice-48331.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
